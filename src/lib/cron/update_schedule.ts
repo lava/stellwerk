0820e1be1ad829faa81e92727aeed4b63bedff6b
ntq <ntq@popret.de>
2024-12-28 15:55:05 +0100

    add 38c3 community stages

--- src/lib/cron/update_schedule.ts
+++ src/lib/cron/update_schedule.ts
@@ -22,29 +22,34 @@ function mapRoomToStreamUrl(room: string): string {
   return "unknown room";
 }
 
+const SCHEDULE_URLS = [
+  "https://fahrplan.events.ccc.de/congress/2024/fahrplan/schedule/export/schedule.json",
+  "https://cfp.cccv.de/38c3-community-stages/schedule/export/schedule.json",
+] as const;
+
 async function main() {
   const vm = new VotingMachine();
-  const BASE_URL =
-    "https://fahrplan.events.ccc.de/congress/2024/fahrplan/schedule/export/schedule.json";
-  const response = await fetch(BASE_URL);
-  const schedule = await response.json();
-  for (const event of parseSchedule(schedule)) {
-    const [hours, minutes] = event.duration.split(":");
-    const duration =
-      (parseInt(hours) * 60 * 60 + parseInt(minutes) * 60) * 1000;
-    vm.upsertStream({
-      id: event.guid,
-      title: event.title,
-      room: event.room,
-      stream_url: mapRoomToStreamUrl(event.room),
-      url: event.url,
-      description: event.description,
-      abstract: event.abstract,
-      start_date: new Date(event.date),
-      end_date: new Date(new Date(event.date).getTime() + duration),
-    });
+
+  for (const scheduleUrl of SCHEDULE_URLS) {
+    const response = await fetch(scheduleUrl);
+    const schedule = await response.json();
+    for (const event of parseSchedule(schedule)) {
+      const [hours, minutes] = event.duration.split(":");
+      const duration =
+        (parseInt(hours) * 60 * 60 + parseInt(minutes) * 60) * 1000;
+      vm.upsertStream({
+        id: event.guid,
+        title: event.title,
+        room: event.room,
+        stream_url: mapRoomToStreamUrl(event.room),
+        url: event.url,
+        description: event.description,
+        abstract: event.abstract,
+        start_date: new Date(event.date),
+        end_date: new Date(new Date(event.date).getTime() + duration),
+      });
+    }
   }
 }
 
 main();
-
