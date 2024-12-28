83493af5bcbadca6c8c64b2253f3693705a905c7
ntq <ntq@popret.de>
2024-12-28 23:26:26 +0100

    hide scrollbars in dialog and app

--- src/routes/chooser/+page.svelte
+++ src/routes/chooser/+page.svelte
@@ -16,95 +16,96 @@
   });
 </script>
 
-<div class="app">
-  <h1>Votes</h1>
+<main>
+  <div class="app">
+    <h1>Votes</h1>
 
-  <h2>Live</h2>
-  <div class="radio-group">
-    {#each data.liveStreams as stream}
-      <form
-        method="POST"
-        action="?/vote"
-        onchange={(e) => e.currentTarget.requestSubmit()}
-      >
-        <input type="hidden" name="choice" value={stream.id} />
-        <div class="radio-panel">
-          <input
-            type="checkbox"
-            name="selected"
-            id={stream.id}
-            checked={data.votes.includes(stream.id) ?? undefined}
-          />
-          <label for={stream.id}>
-            <div>
-              {stream.title}
-              <span
-                onclick={(e) => {
-                  selectedStream = stream;
-                  e.preventDefault();
-                }}
-                class="info-button"
-              >
-                🛈
-              </span>
-            </div>
-
-            <span class="checkmark"
-              >{Array(stream.votes ?? 0)
-                .fill("✓")
-                .join("")}</span
-            >
-          </label>
-        </div>
-      </form>
-    {/each}
-  </div>
-
-  <h2>Upcoming</h2>
-  <div class="radio-group">
-    {#each data.upcomingStreams as stream}
-      <form
-        method="POST"
-        action="?/vote"
-        onchange={(e) => e.currentTarget.requestSubmit()}
-      >
-        <input type="hidden" name="choice" value={stream.id} />
-        <div class="radio-panel">
-          <input
-            type="checkbox"
-            name="selected"
-            id={stream.id}
-            checked={data.votes.includes(stream.id) ?? undefined}
-          />
-          <label for={stream.id}>
-            <span>
-              {stream.title}
-              <span
-                onclick={(e) => {
-                  selectedStream = stream;
-                  e.preventDefault();
-                }}
-                class="info-button"
-              >
-                🛈
-              </span>
-            </span>
-            <div class="panel-footer">
-              <div class="until-start-text">
-                Starts in {formatDistanceToNow(stream.start_date)}
+    <h2>Live</h2>
+    <div class="radio-group">
+      {#each data.liveStreams as stream}
+        <form
+          method="POST"
+          action="?/vote"
+          onchange={(e) => e.currentTarget.requestSubmit()}
+        >
+          <input type="hidden" name="choice" value={stream.id} />
+          <div class="radio-panel">
+            <input
+              type="checkbox"
+              name="selected"
+              id={stream.id}
+              checked={data.votes.includes(stream.id) ?? undefined}
+            />
+            <label for={stream.id}>
+              <div>
+                {stream.title}
+                <span
+                  onclick={(e) => {
+                    selectedStream = stream;
+                    e.preventDefault();
+                  }}
+                  class="info-button"
+                >
+                  🛈
+                </span>
               </div>
-              <div class="checkmark">
-                {Array(stream.votes ?? 0)
+
+              <span class="checkmark"
+                >{Array(stream.votes ?? 0)
                   .fill("✓")
-                  .join("")}
-              </div>
-            </div>
-          </label>
-        </div>
-      </form>
-    {/each}
-  </div>
+                  .join("")}</span
+              >
+            </label>
+          </div>
+        </form>
+      {/each}
+    </div>
 
+    <h2>Upcoming</h2>
+    <div class="radio-group">
+      {#each data.upcomingStreams as stream}
+        <form
+          method="POST"
+          action="?/vote"
+          onchange={(e) => e.currentTarget.requestSubmit()}
+        >
+          <input type="hidden" name="choice" value={stream.id} />
+          <div class="radio-panel">
+            <input
+              type="checkbox"
+              name="selected"
+              id={stream.id}
+              checked={data.votes.includes(stream.id) ?? undefined}
+            />
+            <label for={stream.id}>
+              <span>
+                {stream.title}
+                <span
+                  onclick={(e) => {
+                    selectedStream = stream;
+                    e.preventDefault();
+                  }}
+                  class="info-button"
+                >
+                  🛈
+                </span>
+              </span>
+              <div class="panel-footer">
+                <div class="until-start-text">
+                  Starts in {formatDistanceToNow(stream.start_date)}
+                </div>
+                <div class="checkmark">
+                  {Array(stream.votes ?? 0)
+                    .fill("✓")
+                    .join("")}
+                </div>
+              </div>
+            </label>
+          </div>
+        </form>
+      {/each}
+    </div>
+  </div>
   {#if selectedStream}
     <div class="overlay" onclick={() => (selectedStream = undefined)}>
       <div class="stream-dialog">
@@ -117,19 +118,19 @@
       </div>
     </div>
   {/if}
-</div>
+</main>
 
 <style>
   h1 {
     color: var(--color-text-main);
     font-size: 2rem;
-    margin: 1rem;
+    margin: 1rem 0;
   }
 
   h2 {
     color: var(--color-text-main);
     font-size: 1.5rem;
-    margin: 1rem;
+    margin: 1rem 0;
   }
 
   .radio-group {
@@ -184,41 +185,75 @@
     color: var(--color-active);
   }
 
-  .app {
+  main {
     display: flex;
-    flex-direction: column;
+    padding: 0;
     background-color: black;
-    height: 100%;
-    width: 100%;
-    overflow: auto;
-    color: var(--color-text-main);
-    padding: 1.5rem;
+    margin: 0;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
     box-sizing: border-box;
   }
 
+  .app {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+    color: var(--color-text-main);
+    padding: 1.5rem;
+    box-sizing: border-box;
+    overflow: scroll;
+    scrollbar-width: thin;
+    scrollbar-color: transparent transparent;
+  }
+
+  .app::-webkit-scrollbar {
+    width: 0;
+    background: transparent;
+  }
+
   .overlay {
-    position: fixed;
+    position: absolute;
+    left: 0;
+    right: 0;
     height: 100vh;
     width: 100vw;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    background-color: rgba(0, 0, 0, 0.5);
+    margin: 0;
+    padding: 0;
+    background-color: rgba(0, 0, 0, 0.7);
   }
 
   .stream-dialog {
-    padding: 16px;
+    padding: 2rem;
+    padding-bottom: 3rem;
     border: 1px solid var(--color-border);
     background-color: black;
     min-width: 320px;
     min-height: 180px;
     max-width: 80vw;
     max-height: 80vh;
+    overflow-y: hidden;
   }
 
   .stream-dialog .description {
     white-space: pre-line;
+    text-overflow: ellipsis;
+    overflow: scroll;
+    /* hide scrollbar */
+    scrollbar-width: thin; /* For Firefox */
+    scrollbar-color: transparent transparent; /* For Firefox */
+  }
+
+  /* For WebKit browsers (Chrome, Safari) */
+  .stream-dialog .description::-webkit-scrollbar {
+    width: 0; /* Remove scrollbar space */
+    background: transparent; /* Optional: just to be sure */
   }
 
   .info-button {
